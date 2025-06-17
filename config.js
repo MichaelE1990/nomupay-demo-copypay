4cb6bdb5c308fda5f2ff787a0417097a5272bbd8
Michael Engler <mike@iMac.mynet>
2025-06-17 09:49:44 +0100

    Include config.js so config module is available in production

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+require('dotenv').config();
+
+module.exports = {
+  ENTITY_ID:        process.env.ENTITY_ID,
+  ACCESS_TOKEN:     process.env.ACCESS_TOKEN,
+  API_HOST:         process.env.API_HOST || 'eu-test.oppwa.com',
+  SHOPPER_RESULT_URL: process.env.SHOPPER_RESULT_URL || '/result'
+};
