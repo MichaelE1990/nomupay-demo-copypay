ec86a7c1366c5c3b89ea552542b5709628e14e66
Michael Engler <mike@iMac.mynet>
2025-06-17 11:42:41 +0100

    Allow config.js to be deployed; remove from gitignore

--- public/wpwloptions.js
+++ public/wpwloptions.js
@@ -36,9 +36,6 @@ var wpwlOptions = {
     allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
     merchantName: "Nomupay Demo",
     allowedCardNetworks: ["AMEX", "DISCOVER", "JCB", "MASTERCARD", "VISA"],
-    buttonColor: "white",
-    buttonType: "pay",
-    buttonSizeMode: "fill",
     shippingAddressParameters: {
       allowedCountryCodes: ["US", "IN"],
       phoneNumberRequired: true
