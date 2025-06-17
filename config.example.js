c14453f2069c202f1d2f24ef343f2fdfaa2f0d76
Michael Engler <mike@iMac.mynet>
2025-06-17 10:36:49 +0100

    Allow config.js to be deployed; remove from gitignore

--- config.example.js
+++ config.example.js
@@ -1,11 +1,12 @@
+// config.js example credentials. 
 // Load local .env in non-production environments
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
 module.exports = {
-  ENTITY_ID:        process.env.ENTITY_ID,
-  ACCESS_TOKEN:     process.env.ACCESS_TOKEN,
-  API_HOST:         process.env.API_HOST || 'eu-test.oppwa.com',
+  ENTITY_ID:         process.env.ENTITY_ID,
+  ACCESS_TOKEN:      process.env.ACCESS_TOKEN,
+  API_HOST:          process.env.API_HOST || 'eu-test.oppwa.com',
   SHOPPER_RESULT_URL: process.env.SHOPPER_RESULT_URL || '/result'
 };
