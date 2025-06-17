8e9e2d962f7130cef71dccb5b06a3129a9d01c12
Michael Engler <mike@iMac.mynet>
2025-06-17 12:33:07 +0100

    Update Google Pay options in wpwloptions.js

--- public/wpwloptions.js
+++ public/wpwloptions.js
@@ -36,7 +36,7 @@ var wpwlOptions = {
     allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
     merchantName: "Nomupay Demo",
     allowedCardNetworks: ["AMEX", "DISCOVER", "JCB", "MASTERCARD", "VISA"],
-    buttonColor: "white",
+    buttonColor: "black",
     buttonType: "pay",
     shippingAddressParameters: {
       allowedCountryCodes: ["US", "IN"],
