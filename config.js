3ec96f3c03befb7447c91269172350edf6213843
Michael Engler <mike@iMac.mynet>
2025-08-11 13:28:51 +0100

    change Applepay details

--- config.js
+++ config.js
@@ -1,12 +1,6 @@
-// config.js
-// Load local .env in non-production environments
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-
 module.exports = {
   ENTITY_ID:        process.env.ENTITY_ID,
   ACCESS_TOKEN:     process.env.ACCESS_TOKEN,
-  API_HOST:         process.env.API_HOST || 'eu-test.oppwa.com',
+  API_HOST:         process.env.API_HOST || 'eu-prod.oppwa.com',
   SHOPPER_RESULT_URL: process.env.SHOPPER_RESULT_URL || '/result'
 };
